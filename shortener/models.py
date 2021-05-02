ef3898b0bb09f9ae14b194f512246c43f45a174a
ryan <shlee@madup.com>
2021-05-02 23:45:14 +0900

    Initial Commit

---
+++ shortener/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
