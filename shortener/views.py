e2f1b2cabdef9930a969a38e059bde37028d2033
ryan <shlee@madup.com>
2021-05-16 13:51:09 +0900

    Model View URL II

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,7 @@
+from django.http.response import JsonResponse
 from shortener.models import Users
 from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -7,14 +9,20 @@ from django.shortcuts import redirect, render
 def index(request):
     user = Users.objects.filter(username="admin").first()
     email = user.email if user else "Anonymous User!"
-    print(email)
-    print(request.user.is_authenticated)
-    if request.user.is_authenticated is False:
-        email = "Anonymous User!"
-    print(email)
-    return render(request, "base.html", {"welcome_msg": "Hello FastCampus!"})
+    return render(request, "base.html", {"welcome_msg": f"Hello {email}", "hello": "world"})
 
 
-def redirect_test(request):
-    print("Go Redirect")
-    return redirect("index")
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+
+        return JsonResponse(dict(msg="You just reached with Post Method!"))
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from shortener.views import index, redirect_test
+from shortener.views import index, get_user
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path("redirect", redirect_test),
+    path("get_user/<int:user_id>", get_user),
 ]
