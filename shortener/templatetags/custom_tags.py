18832cffd129c9e6a565a3ca4fc673c40f381acf
ryan <shlee@madup.com>
2021-07-30 23:54:28 +0900

    Login HTML Publishing

--- shortener/templatetags/custom_tags.py
+++ shortener/templatetags/custom_tags.py
@@ -7,16 +7,14 @@ register = template.Library()
 
 
 @register.filter(name="email_ma")
-def email_masker(value):
+def email_masker(value, arg):
     email_split = value.split("@")
-    return f"{email_split[0]}@******.***"
+    return f"{email_split[0]}@******.***" if arg % 2 == 0 else value
 
 
 @register.simple_tag(name="test_tags", takes_context=True)
 def test_tags(context):
-    for c in context:
-        print(c)
+    
     tag_html = "<span class='badge badge-primary'>테스트 태그</span>"
 
     return mark_safe(tag_html)
-
--- shortener/views.py
+++ shortener/views.py
@@ -55,20 +55,26 @@ def register(request):
 
 
 def login_view(request):
+    msg = None
+    is_ok = False
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
-        msg = "가입되어 있지 않거나 로그인 정보가 잘못 되었습니다."
         if form.is_valid():
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=raw_password)
             if user is not None:
-                msg = "로그인 성공"
                 login(request, user)
-        return render(request, "login.html", {"form": form, "msg": msg})
+                is_ok = True
+        else:
+            msg = "올바른 유저ID와 패스워드를 입력하세요."
     else:
         form = AuthenticationForm()
-        return render(request, "login.html", {"form": form})
+
+    for visible in form.visible_fields():
+        visible.field.widget.attrs["placeholder"] = "유저ID" if visible.name == "username" else "패스워드"
+        visible.field.widget.attrs["class"] = "form-control"
+    return render(request, "login.html", {"form": form, "msg": msg, "is_ok": is_ok})
 
 
 def logout_view(request):
